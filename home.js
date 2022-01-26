ead13147fcba96feda0de767215c5b65d414bd99
unknown <moraba@.com>
2022-01-26 10:23:55 +0200

    added console password

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 function login () {
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    console.log(password)
+    console.log(password);
 
-    
+
 }
