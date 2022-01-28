7e53c6945ef841d3e95c8abe0c0ea7a0d95b0a67
unknown <moraba@.com>
2022-01-28 10:27:14 +0200

    added login

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 const cars = ["Saab", "Volvo", "BMW","ford","izuzu","rental","mercden","aduio","bently","chervolet","fiat","dogge","honda","jeep","kia"];
 const display = document.getElementById("display");
+
 for (let i =0; i <cars.length; i ++ ) {
     display.innerHTML += "<ol>" + cars[i] + "</ol>";
 
